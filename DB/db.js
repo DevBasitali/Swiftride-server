427d4be55ad4ce5660450e6a9da98bdfd6ea55b3
syedafatima26 <syedafatimamansoor@gmail.com>
2025-03-10 23:43:04 +0500

    chaning

--- DB/db.js
+++ DB/db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose"
-const dbconnect=async (server)=>{
-    await mongoose.connect(process.env.MONGO_URI)
+const dbconnect=async (app)=>{
+    console.log("Loading ENV variables...");
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+    console.log("PORT:", process.env.PORT);
+    mongoose.connect('mongodb://127.0.0.1:27017/rentrush', {
+        serverSelectionTimeoutMS: 60000
+    })
     .then(()=>{
-        server.listen(process.env.PORT, ()=>{
-    console.log("listing port",process.env.MONGO_URI, process.env.PORT)
+    app.listen(3000, ()=>{
+    console.log("listing port ...",process.env.MONGO_URI, process.env.PORT)
    
     })})
     .catch(err=>console.error(err))
