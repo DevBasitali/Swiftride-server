3e57e1911c8012ce340141e5b9d6c813ddc19b42
DevFawadKhan <mfawadkhan05@gmail.com>
2025-05-05 19:33:33 +0500

    fixed booking module

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -69,8 +69,9 @@ export const bookCar = async (req, res) => {
     now.setHours(0, 0, 0, 0);
     const CurrentDate = new Date();
     const userDate = new Date(`${rentalStartDate}`);
-    
-    if (userDate < CurrentDate) {
+    const datePart = CurrentDate.toISOString().split("T")[0];
+     const userTime = new Date(`${datePart} ${rentalStartTime}`);
+    if (userTime < CurrentDate) {
       return res
         .status(400)
         .json({ message: "Rental Start time must be in future" });
